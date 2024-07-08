0a75258ccf88de2c7a6443f6b803d2c24eeb85a3
harshitsahush <harshitsahu.sh@gmail.com>
2024-07-08 15:30:15 +0530

    Added JSON templates to fix the schema of the response

--- utils.py
+++ utils.py
@@ -1,7 +1,28 @@
 from PyPDF2 import PdfReader
 from groq import Groq
 from docx import Document
+import json
+from pydantic import BaseModel
 
+
+class Education(BaseModel):
+    degree : str
+    university : str
+    grad_date : str
+
+class Experience(BaseModel):
+    job_title: str
+    company : str
+    duration : str
+    responsibilities : list[str]
+
+class Response(BaseModel):
+    name : str
+    email : str
+    contact : str
+    skills : list[str]
+    education : list[Education]
+    experiences : list[Experience]
 
 def get_pdf_text(pdfs):
     text = ""
@@ -26,22 +47,17 @@ def query_response(text):
         messages = [
             {
                 "role" : "system",
-                "content" : """You are a resume extractor API that responds in JSON. The provided text is a resume and the JSON schema should include 
-                            {
-                                "Name" :  The name of the applicant ,
-                                "email" : The email of the applicant,
-                                "phone_number" : The phone number of the applicant,
-                                "skills" : the skills of the applicant in the form of a list containing strings,
-                                "education" : education of the applicant
-                                "experience" : experience of the applicant
-                            } In case no relevant data is found for a key, keep it null."""
+                "content" : """You are a resume extractor API that responds in JSON. The provided text is a resume and the response must use the schema \n """ +
+                json.dumps(Response.model_json_schema(), indent = 2)
             },
             {
                 "role" : "user",
                 "content" : text
             }
         ],
-        model = "llama3-70b-8192"
+        model = "llama3-70b-8192",
+
+        response_format={"type" : "json_object"}
     )
 
     return chat_completion.choices[0].message.content
